1a31bc713358d613227858ceb92055f1fd101b8c
Sakila <sakilalakshmi95@gmail.com>
2023-08-31 16:33:49 +0530

    palindrome

--- 02-sample.sh
+++ 02-sample.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-output=$(cat /d/sample.txt | tr ' ' '\n' | sort | uniq -c | awk '{print $2"@"$1}')
+file=$(touch sample.txt)
+
+output=$(cat sample.txt | tr ' ' '\n' | sort | uniq -c | awk '{print $2"@"$1}')
 echo "$output"
