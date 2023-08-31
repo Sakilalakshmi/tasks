8b8adbd41698a41e5acee6ff8ef01503dcbc4f5a
Sakila <sakilalakshmi95@gmail.com>
2023-08-31 16:30:37 +0530

    palindrome

--- 02-sample.sh
+++ 02-sample.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file_path = "D/sample.txt"
+file_path ="$(D/sample.txt)"
 output=$(cat $file_path % tr ' ' '\n' | sort | uniq -c | \
 awk '{print $2"@"$1}' > myFile.txt)
 echo "$ouput"
